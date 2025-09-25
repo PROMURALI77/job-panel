c05654e8c375146d73e2a8c28c0dd0b3741f2e88
FORGE HUSTLE <progamer32051@gmail.com>
2025-09-25 16:03:29 +0530

    chore : eslint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,11 +18,8 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
+      "app/**"
     ],
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
   },
 ];
 
